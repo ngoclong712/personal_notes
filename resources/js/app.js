186b107c0ada6269c13890fa0f471bf0802da829
Ngọc Long <143398861+ngoclong712@users.noreply.github.com>
2025-09-12 23:48:31 +0700

    vuejs initialize & database v1
    1. Different between v-if and v-show
    2. v-for with index (start from 0)
    3. v-for with key - value (value before key)
    4. $event
    5. Modifiers on event (.stop, .once, .prevent, ...)
    6. Could use v-on in div also, not only button
    7. Modifiers on model (.trim, .lazy, .number, ...)
    8. Computed help data not re render for more efficient
    9. Watch (oldValue, newValue)
    10. Global import (main.js) and local import (app.vue)
    11. defineProps
    12. emit Event: send event from child component into parent component (contradictory with props)
    13. Provide and inject

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+import { createApp } from 'vue';
+
+import ExampleComponent from "@/components/ExampleComponent.vue";
+
+const app = createApp({})
+
+app.component(ExampleComponent.name, ExampleComponent);
+
+app.mount('#app')
