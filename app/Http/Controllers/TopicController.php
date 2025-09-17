debf854bfffc035eb89bd7a7c18c082546917e68
Ngọc Long <143398861+ngoclong712@users.noreply.github.com>
2025-09-17 19:29:10 +0700

    Update TopicController.php

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Topic;
 use Illuminate\Http\Request;
 
 class TopicController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json(Topic::all());
+    }
 }
