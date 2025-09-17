4d37a136f1c36996a6f91806913d3cb3b70f55b5
Ngọc Long <143398861+ngoclong712@users.noreply.github.com>
2025-09-17 21:59:57 +0700

    Topic Request & Controller

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -2,13 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Topic\StoreRequest;
+use App\Http\Requests\Topic\UpdateRequest;
 use App\Models\Topic;
-use Illuminate\Http\Request;
 
 class TopicController extends Controller
 {
+    use ResponseTrait;
+
     public function index()
     {
-        return response()->json(Topic::all());
+        $topics = Topic::query()
+            ->orderByDesc('id')
+            ->get(['id','name','description','created_at']);
+
+        return $this->successResponse($topics);
+    }
+
+    public function show(Topic $topic)
+    {
+        return $this->successResponse($topic->only(['id','name','description']));
+    }
+
+    public function store(StoreRequest $request)
+    {
+        $topic = Topic::create($request->validated());
+        return $this->successResponse($topic->only(['id','name','description']), 'Created');
+    }
+
+    public function update(Topic $topic, UpdateRequest $request)
+    {
+        $topic->update($request->validated());
+        return $this->successResponse($topic->only(['id','name','description']), 'Updated');
+    }
+
+    public function destroy(Topic $topic)
+    {
+        $topic->delete();
+        return $this->successResponse([], 'Deleted');
     }
 }
