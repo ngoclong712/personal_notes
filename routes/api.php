da661a358bdfd3bde7d854ccd3e4594dcafbc955
Ngọc Long <143398861+ngoclong712@users.noreply.github.com>
2025-09-17 22:02:11 +0700

    update route backend

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 //Topic
 Route::get('/topics', [TopicController::class, 'index'])->name('topics');
+Route::get('/topics/{topic}', [TopicController::class, 'show'])->name('topics.show');
+Route::post('/topics', [TopicController::class, 'store'])->name('topics.store');
+Route::put('/topics/{topic}', [TopicController::class, 'update'])->name('topics.update');
+Route::delete('/topics/{topic}', [TopicController::class, 'destroy'])->name('topics.destroy');
