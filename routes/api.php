9b8b42da3bd75fb04f7d0a70daa1788cf76599f2
Ngọc Long <143398861+ngoclong712@users.noreply.github.com>
2025-09-23 15:59:39 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\NoteController;
 use App\Http\Controllers\TopicController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,11 @@ Route::get('/topics/{topic}', [TopicController::class, 'show'])->name('topics.sh
 Route::post('/topics', [TopicController::class, 'store'])->name('topics.store');
 Route::put('/topics/{topic}', [TopicController::class, 'update'])->name('topics.update');
 Route::delete('/topics/{topic}', [TopicController::class, 'destroy'])->name('topics.destroy');
+Route::post('/topics/import', [TopicController::class, 'import'])->name('topics.import');
+
+Route::get('/notes', [NoteController::class, 'index'])->name('notes');
+Route::get('/notes/{note}', [NoteController::class, 'show'])->name('notes.show');
+Route::post('/notes', [NoteController::class, 'store'])->name('notes.store');
+Route::put('/notes/{note}', [NoteController::class, 'update'])->name('notes.update');
+Route::delete('/notes/{note}', [NoteController::class, 'destroy'])->name('notes.destroy');
+Route::post('/notes/import', [NoteController::class, 'import'])->name('notes.import');
