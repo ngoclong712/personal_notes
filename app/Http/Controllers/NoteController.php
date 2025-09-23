de55847bc5edf18fa8176eb0cb12957e2ef1db2e
Ngọc Long <143398861+ngoclong712@users.noreply.github.com>
2025-09-23 15:59:47 +0700

    note index

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Note;
 use Illuminate\Http\Request;
 
 class NoteController extends Controller
 {
-    //
+    use ResponseTrait;
+    public function index()
+    {
+        $notes = Note::query()
+            ->with('topic:id,name')
+            ->orderByDesc('id')
+            ->get(['id','title','status','updated_at','topic_id']);
+
+        return $this->successResponse($notes);
+    }
 }
