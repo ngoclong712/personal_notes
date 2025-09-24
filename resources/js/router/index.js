711003b3050eca67b9b106af4e19b15290855ceb
Ngọc Long <143398861+ngoclong712@users.noreply.github.com>
2025-09-24 23:17:15 +0700

    update router

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,13 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MainLayout from "@/layouts/MainLayout.vue";
 import BareLayout from "@/layouts/BareLayout.vue";
+
 import Home from '@/views/Home.vue'
 import Note from "@/views/Note.vue";
 import Deadline from "@/views/Deadline.vue";
 import Topic from "@/views/Topic.vue";
+
 import AddTopic from "@/views/topics/AddTopic.vue";
 import TopicDetail from "@/views/topics/TopicDetail.vue";
 import EditTopic from "@/views/topics/EditTopic.vue";
+
+import AddNote from "@/views/notes/AddNote.vue";
+import EditNote from "@/views/notes/EditNote.vue";
+import NoteDetail from "@/views/notes/NoteDetail.vue";
+
 import Test from "@/views/Test.vue";
 
 const routes = [
@@ -60,6 +67,21 @@ const routes = [
                 path: 'topic/:id/edit',
                 name: 'topic.edit',
                 component: EditTopic,
+            },
+            {
+                path: 'note/add',
+                name: 'note.add',
+                component: AddNote,
+            },
+            {
+                path : 'note/:id',
+                name: 'note.detail',
+                component: NoteDetail,
+            },
+            {
+                path: 'note/:id/edit',
+                name: 'note.edit',
+                component: EditNote,
             }
         ]
     }
