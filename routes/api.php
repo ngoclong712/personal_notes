66b0bc58010d463344343e92b7594f75fbee8926
Ngọc Long <143398861+ngoclong712@users.noreply.github.com>
2025-10-06 21:29:56 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DeadlineController;
 use App\Http\Controllers\NoteController;
 use App\Http\Controllers\TopicController;
 use Illuminate\Support\Facades\Route;
@@ -18,3 +19,9 @@ Route::post('/notes', [NoteController::class, 'store'])->name('notes.store');
 Route::put('/notes/{note}', [NoteController::class, 'update'])->name('notes.update');
 Route::delete('/notes/{note}', [NoteController::class, 'destroy'])->name('notes.destroy');
 Route::post('/notes/import', [NoteController::class, 'import'])->name('notes.import');
+
+Route::get('/deadlines', [DeadlineController::class, 'index'])->name('deadlines');
+Route::get('/deadlines/{id}', [DeadlineController::class, 'show'])->name('deadlines.show');
+Route::post('/deadlines', [DeadlineController::class, 'store'])->name('deadlines.store');
+Route::put('/deadlines/{id}', [DeadlineController::class, 'update'])->name('deadlines.update');
+Route::delete('/deadlines/{id}', [DeadlineController::class, 'destroy'])->name('deadlines.destroy');
